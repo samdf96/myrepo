c43408ad2ba804ebf315f1805904e7feb99ce545
samdf96 <sdf7@telus.net>
2018-01-16 00:58:47 +0000

    Updates Jan 15 Late

--- Assignment_1/Basic_Plots.py
+++ Assignment_1/Basic_Plots.py
@@ -35,26 +35,26 @@ ad = ds.all_data()
 #Creating my Speed Derived Variable for Computations and Plotting
 
 def rms_speed(field, data):
-    return ((data['gas', 'velocity_x']**2) * (data['gas', 'velocity_y']**2) * (data['gas', 'velocity_z']**2))**(1/2)
+    return ((data['gas', 'velocity_x']**2) + (data['gas', 'velocity_y']**2) + (data['gas', 'velocity_z']**2))**(1/2)
 
-ds.add_field(("gas", "rms_speed"), units="cm**3/s**3", function=rms_speed)
+ds.add_field(("gas", "rms_speed"), units="cm/s", function=rms_speed)
 #Re-defining all data to include added rms_speed
 ad = ds.all_data()
 
 #Plotting 2D PDF
-'''
+
 plot_2D_PDF = yt.PhasePlot(ad, "density", "rms_speed", "cell_mass",
                     weight_field=None, fractional=True)
 
 plot_2D_PDF.save("2D_PDF_Density_RMS_Speed.png")
-    '''
+    
 
 #Computations and Data Analysis
-
+'''
 #Computing Total Mass in Domain
 print("Total Mass of Gas in Domain = ", ad.quantities.total_quantity([("gas", "cell_mass")]))
 print("Total Kinetic Energy in Domain = ", ad.quantities.total_quantity([("gas", "kinetic_energy")]))
 print("Total Momentum in x-direction in Domain = ", ad.quantities.total_quantity([("gas", "momentum_x")]))
-
+'''
 
 
