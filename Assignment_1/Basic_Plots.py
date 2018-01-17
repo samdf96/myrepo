006f7f4077e2f8292bb023cc4d87f0091f2b5fb4
samdf96 <sdf7@telus.net>
2018-01-17 05:40:06 +0000

    Jan 16 PM Updates

--- Assignment_1/Basic_Plots.py
+++ Assignment_1/Basic_Plots.py
@@ -9,10 +9,13 @@ Created on Fri Jan 12 05:01:24 2018
 import yt as yt
 #Loading Data from file.
 ds = yt.load("~/bigdata/data.0453.3d.hdf5")
-ds2 = yt.load("~/bigdata/chk.0453.3d.sink")
+ds2 = yt.load("~/bigdata/chk.0453.3d.hdf5")
+
+
 #Printing Derived Variables to be able to use
-for i in sorted(ds2.derived_field_list):
+for i in sorted(ds.derived_field_list):
     print(i)
+
 
 
 #Creating Slice Plots
@@ -39,7 +42,9 @@ def rms_speed(field, data):
     return ((data['gas', 'velocity_x']**2) + (data['gas', 'velocity_y']**2) + (data['gas', 'velocity_z']**2))**(1/2)
 
 ds.add_field(("gas", "rms_speed"), units="cm/s", function=rms_speed)
+
 #Re-defining all data to include added rms_speed
+
 ad = ds.all_data()
 
 #Plotting 2D PDF
@@ -50,12 +55,27 @@ plot_2D_PDF = yt.PhasePlot(ad, "density", "rms_speed", "cell_mass",
 plot_2D_PDF.save("2D_PDF_Density_RMS_Speed.png")
 '''
 
+#Plotting 1D PDF
+plot_1D_energy = yt.ProfilePlot(ad,"kinetic_energy","density")
+plot_1D_energy.save("1D_PDF_Energy.png")
+plot_1D_mass = yt.ProfilePlot(ad,"cell_mass","density")
+plot_1D_mass.save("1D_PDF_Mass.png")
+
+#Plotting Mean Velocity Projection Weighted
+'''
+plot_weighted_projection_z = yt.ProjectionPlot(ds,"z","rms_speed",weight_field="cell_mass")
+plot_weighted_projection_z.save("2D_Projection_Weighted_RMS_Speed_z.png")
+plot_weighted_projection_x = yt.ProjectionPlot(ds,"x","rms_speed",weight_field="cell_mass")
+plot_weighted_projection_x.save("2D_Projection_Weighted_RMS_Speed_x.png")
+plot_weighted_projection_y = yt.ProjectionPlot(ds,"y","rms_speed",weight_field="cell_mass")
+plot_weighted_projection_y.save("2D_Projection_Weighted_RMS_Speed_y.png")
+'''
 #Computations and Data Analysis
-'''
+
 #Computing Total Mass in Domain
-print("Total Mass of Gas in Domain = ", ad.quantities.total_quantity([("gas", "cell_mass")]))
-print("Total Kinetic Energy in Domain = ", ad.quantities.total_quantity([("gas", "kinetic_energy")]))
-print("Total Momentum in x-direction in Domain = ", ad.quantities.total_quantity([("gas", "momentum_x")]))
-'''
+#print("Total Mass of Gas in Domain = ", ad.quantities.total_quantity([("gas", "cell_mass")]))
+#print("Total Kinetic Energy in Domain = ", ad.quantities.total_quantity([("gas", "kinetic_energy")]))
+#print("Total Momentum in x-direction in Domain = ", ad.quantities.total_quantity([("gas", "momentum_x")]))
+
 
 
