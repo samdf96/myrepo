88a6749129f2803cdcf0948dc3f0fd94a25a13f8
samdf96 <sdf7@telus.net>
2018-01-24 18:48:53 +0000

    Jan 24 Noon Updates

--- Assignment_2/0060/Jan_19_Plotting.py
+++ Assignment_2/0060/Jan_19_Plotting.py
@@ -5,9 +5,32 @@ Created on Fri Jan 19 19:53:35 2018
 
 @author: sfielder
 """
+# This script will create the following:
+#   Projection Plots:
+#                   Mass Density along z-axis
+#                   Angular momentum (x and y) along z-axis
+#                   Velocity (z direction) along z-axis
+#   PDF's:
+#                   Mass Density
+#
+# This script will also map the velocity field onto a plane to extract
+# the x and y gradients, and their magnitude is extracted and saved
+# in a csv which can be ported into Latex directly into a table
+# which follows exactly the subregions that the original data was split to
+
+'''
+Will need to set a few values in the script for differing data sets:
+    Filename for importing data
+    Data Length (Number of pixels along a given axis for original data set)
+    Array_split_size - number of pixels per subregion along a given axis
+    Array_split_size_sub - number of subregions in total
+'''
+
 
 # Import Packages here.
 import yt as yt
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Call different data sets here, possibly create loop.
 ds = yt.load("~/bigdata/Fiducial00/data.0060.3d.hdf5")
@@ -15,13 +38,12 @@ ds = yt.load("~/bigdata/Fiducial00/data.0060.3d.hdf5")
 #Loading all data into one object.
 ad = ds.all_data()
 
-#Printing Derived Variables to be able to use (comment in if needed)
+#Printing Derived Variables to be able to use (comment in if needed).
 '''
 for i in sorted(ds.derived_field_list):
     print(i)
 '''
 
-'''
 # Basic Projection Plots
 
 #Density Projection along z
@@ -44,31 +66,31 @@ z_projected_z_velocity.save("z_velocity_density_plot.png")
 
 plot_1D_PDF_Density = yt.ProfilePlot(ad, "density", "ones", weight_field=None)
 plot_1D_PDF_Density.save("1D_PDF_Density.png")
-'''
 
+##############################################################################
 
+#Analysis of Data Section
 
-import matplotlib.pyplot as plt
 
 # This creates a projection weighting by density
 vz = ad.integrate('velocity_z',weight='density',axis='z')
 
-# The projection has fields 'px' and 'py' for the position
-# and whatever quantity you average over like velocity_z.
+# The projection has fields 'px' and 'py' for the position.
 
 plt.scatter(vz['px'],vz['py'],c=vz['velocity_z'])
 plt.colorbar()
 plt.show()
 
-
+#Defining Value for underlying data set length
+data_length = 256
 arr = vz['velocity_z']
-arr.shape = (256,256)  # Since the underlying data are 256^3
-import numpy as np
+arr.shape = (data_length,data_length)  # Since the underlying data are 256^3
+
 arr = np.array(arr)  # Make this into a plain numpy array if needed.
 plt.imshow(arr)      
 plt.show()
 
-#Generate Subsections of Arrays (16 regions of 64x64).
+#Generate Subsections of Arrays.
 def blockshaped(arr, nrows, ncols):
     """
     Return an array of shape (n, nrows, ncols) where
@@ -82,35 +104,55 @@ def blockshaped(arr, nrows, ncols):
                .swapaxes(1,2)
                .reshape(-1, nrows, ncols))
 
-
-#This creates a list of arrays, specifically a list of 16 subregions.
-    # This makes the index number go left to right and top to bottom.
+#This creates a list of arrays, specifically a list of 'x' subregions.
+# This makes the index number go left to right and top to bottom.
     
-array_split_size = 64 #Value for blocks 
+array_split_size = 64 #Value for blocks
+array_split_size_sub = 16
+array_split_size_sub_tot = array_split_size // array_split_size_sub
 arr_reshape = blockshaped(arr,array_split_size,array_split_size)
 #Callable Sizing Value
 array_size = np.size(arr_reshape[0])
 #Plane Fitting Process
     
-    #Creating x,y coordintes for blocks of data
-xvalues = np.arange(0,256);
-yvalues = np.arange(0,256);
+#Creating x,y coordintes for blocks of data
+xvalues = np.arange(0,data_length);
+yvalues = np.arange(0,data_length);
 
 xx, yy = np.meshgrid(xvalues, yvalues)
 
 xx = blockshaped(xx,array_split_size,array_split_size)
 yy = blockshaped(yy,array_split_size,array_split_size)
 
-print(np.ndarray.flatten(xx[0]))
-print(np.ndarray.flatten(yy[0]))
-
-
 #Importing Plane Fit Script here.
 from plane_fit import plane_fit
 
 #Making z of zeros for plane fit script
-first_plane_sec = np.zeros((16,1,3))
+first_plane_sec = np.zeros((array_split_size_sub,1,3))
 
 
-for i in range(0,15):
+for i in range(0,array_split_size_sub):
     first_plane_sec[i] = plane_fit(np.ndarray.flatten(xx[i]),np.ndarray.flatten(yy[i]),np.ndarray.flatten(arr_reshape[i]))
+
+#Need Conversion for values in km/s/pc instead of cm/s/pixel
+conv_length = 1/(10000) #cm to km
+conv_distance = 1/0.039 #pixel to pc
+
+#Grabs matrix / row / column.
+
+
+for i in range(0,array_split_size_sub):
+    for j in range(1,3):
+        first_plane_sec[i,0,j] = first_plane_sec[i,0,j]*conv_length*conv_distance
+    
+#Now computing magnitudes of gradient into empty superset array as defined below
+
+gradients = np.zeros((16,1))
+
+for i in range(0,array_split_size_sub):
+    gradients[i,0] = ((first_plane_sec[i,0,1]**2) + (first_plane_sec[i,0,2]**2))**(1/2)
+    
+#Reshaping matrix for correct position to original plot scheme
+gradients = np.reshape(gradients,(array_split_size_sub_tot,array_split_size_sub_tot))
+
+np.savetxt("gradient_magnitudes_0060.csv",gradients,delimiter=' & ', fmt='%.4g', newline=' \\\\\n')
