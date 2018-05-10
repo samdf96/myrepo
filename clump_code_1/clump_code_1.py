3f534dce7a156c6b8f7d17216731b5ba877af2b3
Samuel Fielder <sdf7@telus.net>
2018-05-10 12:48:26 -0600

    May 10th Noon Updates

--- clump_code_1/clump_code_1.py
+++ clump_code_1/clump_code_1.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -6,6 +7,7 @@ Created on Thu May  3 19:53:52 2018
 @author: sfielder
 """
 
+#%%
 #Importing packages here
 import yt
 import numpy as np
@@ -13,15 +15,17 @@ import astropy
 from yt.analysis_modules.level_sets.api import *
 import math as m
 import sys
+import matplotlib.pyplot as plt
 
 
 #filename = sys.argv[1]
 
-#Defining Global Variables Here
+#Defining Global Variables Here - Move to File After
 l=10	#Length of Original Data Set
 cmin = 5e-21	#Minimum Density Threshold for Clump Finding
-cmax = 5e-22	#Maximum Density Threshold for Clump Finding
-step = 1000	#Step-size multiplier for Clump Finding
+#cmax = 5e-20	#Maximum Density Threshold for Clump Finding
+step = 100	#Step-size multiplier for Clump Finding
+
 
 
 def octant_split(data_object):
@@ -67,18 +71,171 @@ def clump_finder(master_clump,clump_sizing):
     master_clump.add_info_item("center_of_mass") #Adds Center of Mass info for Clumps
     #Finding Clumps Here
     find_clumps(master_clump, cmin, cmax, step)
-    lc = get_lowest_clumps(master_clump)
-    return(lc)
+    leaf_clumps = get_lowest_clumps(master_clump)
+    return(leaf_clumps)
 
-def center_of_mass(lowest_clump):
-    com = lowest_clump.quantities.center_of_mass()
+def center_of_mass(lc):
+    '''
+    Computes the center of mass of a imputted data object
+    Input: Lowest Leaf Clump Data Object
+    Output: 3-list of x,y,z coordinates of center of mass
+    '''
+    com = np.array(lc.quantities.center_of_mass())
     return(com)
 
-ds = yt.load("~/bigdata/Fiducial00/data.0100.3d.hdf5")
+def bounding_box(lc):
+    '''
+    Writes the lower and upper bounds for each axis of a box, given a data
+    clump object
+    
+    Input: Lowest Lead Clump Data Object
+    Output: (3,2)-list of x,y,z min/max values.
+    '''
+    bounding_box = np.array(lc.quantities.extrema([('gas','x'),('gas','y'),('gas','z')]))
+    return(bounding_box)
+    
+def clump_box(data_object,br):
+    '''
+    Takes original data set, and constructs a data object according to
+    the bounding box given.
+    Input:  Original Data in File (ds)
+            br: bounding region of the clump
+    Output: Data object with is constructed according to bounding box,
+            represents a data object around a clump
+    '''
+    dbox_clump = data_object.r[(br[0,0],'cm'):(br[0,1],'cm'), (br[1,0],'cm'):(br[1,1],'cm'), (br[2,0],'cm'):(br[2,1],'cm')]
+    return(dbox_clump)
+    
+def velocity_array(data_object,velocity,axis):
+    '''
+    Takes Data object and integrates velocity along chosen line of sight
+    and returns an array that matches data structure of original data set
+    with only the clump having values in the array, all others are nan
+    
+    Input:  Data object (clump or otherwise)
+        Both next imputs have to be in strings in a specific way
+        Velocity must be: 'velocity_x', 'velocity_y', 'velocity_z'
+        Axis must be: 'x', 'y', 'z'
+            Velocity: the component of velocity to be integrated
+            Axis: Line of sight along which velocity component is integrated
+    '''
+    vz = data_object.integrate(velocity, weight='density', axis=axis)
+    v_reform = vz.to_frb((l,'pc'),(master_dist_data,master_dist_data))
+    v_arr = np.array(v_reform[velocity])
+    v_arr = np.reshape(v_arr,(master_dist_data,master_dist_data))
+    return(v_arr)
+    
+def velocity_array_reducer(velocity_integrated_array):
+    '''
+    Takes velocity integrated array that is in the original dimension size
+    with imbedded data values surrounded by nan values
+    and reduces that array into just the imbedded data,
+    keeping the generally shape of the data intact.
+    Input: Integrated Velocity Array
+    Output:
+        velocity_integrated_array_reduced: just the significant data, no nan values
+        v_pi: First perpendicular coordinate values to the integrated array
+        v_pj: Second perpendicular coordinate values to the integrated array
+        
+        Values for coordinate system are always defined such that the relationship
+        between the integrated array and the perpendicular coordinates
+        follow a cyclical pattern
+        e.g. Calling Z-LOS array, gives first coordinate x, and second coordinate y
+    '''
+    #Find where the data is non-nan valued
+    v_positions = np.argwhere(~np.isnan(velocity_integrated_array))
+    
+    v_positions_ij = []    #Creating list for array slicing
+    v_positions_ij.append(v_positions[0,0]) #First Row Value
+    v_positions_ij.append(v_positions[-1,0]) #Last Row Value
+    v_positions_ij.append(v_positions[0,1])    #First Column Value
+    v_positions_ij.append(v_positions[-1,1])   #Last Column Value
+    
+    #For Creation of x-coords,y-coords
+    v_i_values = np.arange(1,(v_positions_ij[1]-v_positions_ij[0]+2))
+    v_j_values = np.arange(1,(v_positions_ij[3]-v_positions_ij[2]+2))
+    
+    v_jj, v_ii = np.meshgrid(v_i_values, v_j_values)
+    
+    v_pi = np.array(v_ii)
+    v_pj = np.array(v_jj)
+    
+    #Extracts the data into a new numpy array for plane fitting
+    velocity_integrated_array_reduced = velocity_integrated_array[v_positions_ij[0]:v_positions_ij[1]+1,v_positions_ij[2]:v_positions_ij[3]+1]
+    return(velocity_integrated_array_reduced,v_pi,v_pj)
+    
+    
+
+#Loads data into File
+ds = yt.load("data.0060.3d.hdf5")
+master_dist_data = int(ds.domain_dimensions[0])
+
+#Creates a Data Object containing all the Simulation Data
 ad = ds.all_data()
-octants = octant_split(ad)
-master_clump_1 = master_clump_maker(octants[0])
-clump_1 = clump_finder(master_clump_1,150)
-#center_of_mass_1 = center_of_mass(clump_1)
+
+#Splits the Data into Octants
+octant = octant_split(ad)
+
+#Grabs each of the octants and runs them through the Clump Finding algorithm
+clumps = [] #Defining Empty List for loop
+for i in range(4,len(octant)):
+    master_clump_main = master_clump_maker(octant[i])
+    cmax = octant[i]["gas", "density"].max()
+    print("Now Finding clumps for Octant:",i)
+    lc = clump_finder(master_clump_main,30)
+    for j in range(0,len(lc)):
+        clumps.append(lc[j])
+
+# =============================================================================
+# Making empty arrays to store the data, variable to allow for different
+# size of clumps list. Actual array (1,3) and (3,2) will not change as
+# that is how they are written with no difference between clump objects.
+# =============================================================================
+
+com = np.zeros((len(clumps),1,3))
+bregion = np.zeros((len(clumps),3,2))
+
+# Creates lists for center_of_mass of clump list -> stores in com
+# Creates list for bounding boxes of clump list -> stores in bregion
+for i in range(0,len(clumps)):
+    com[i] = center_of_mass(clumps[i])
+    bregion[i] = bounding_box(clumps[i])
+
+
+
+# =============================================================================
+# CHECK IN POINT: What we have so far here in the script
+#
+#     com: arrays that contain x,y,z center of mass values for clumps
+#     bregion: x,y,z min/max value to build the boxes for clumps
+# =============================================================================
+
+
+data_object_clump = [] #Setting Empty list for loop
+#Creates list of data objects for all the clumps found through algorithm
+for i in range(0,len(bregion)):
+    data_object_clump.append(clump_box(ds,bregion[0]))
+
+
+# =============================================================================
+# CHECK IN POINT: What we have so far here in the script
+#
+# list known as data_object_clump which has each of the regions found by the 
+# clumping algorithm.
+# =============================================================================
+    
+
+
+#%%
+
+#Testing: Calling the velocity_array definition
+arr_z = velocity_array(data_object_clump[0],'velocity_z','z')
+arr_x = velocity_array(data_object_clump[0],'velocity_x','x')
+
+arr_z_test,vz_px,vz_py = velocity_array_reducer(arr_z)
+arr_x_test,vx_py,vx_pz  = velocity_array_reducer(arr_x)
+
+
+
 
 
