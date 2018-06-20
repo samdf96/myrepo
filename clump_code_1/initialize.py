9ef6418a1e79f2a556049fd66a6f08824a06059d
Samuel Fielder <sdf7@telus.net>
2018-06-20 10:53:49 -0600

    full push for code changes to directory management

--- clump_code_1/initialize.py
+++ clump_code_1/initialize.py
@@ -29,39 +29,41 @@ for i in range(0,len(flist)):
         flist_data.append(flist[i])
     print('Directory: ' + flist[i] + ' was filtered out.')
 
+#Sorting the Data by filename
+flist_data.sort()
+
 #This is where the config files are
 data_dir = '/home/sfielder/Documents/Clumps/'
 
 #Make a list of all the config files found in  data_dir
 flist_config = glob.glob(data_dir+'*.yaml')
 
-"""
+
 for i in range(0,len(flist)):
     for j in range(0,len(flist_config)):
         naming_string = str(j)
         
         #Text String for Output Directory
-        save_dir = data_dir+'Output'+ naming_string +'/'
+        save_dir = data_dir+'Output_'+ naming_string + '/'
         
         if os.path.isdir(save_dir) == True:
             print("Warning!!! Directory: " +
                   save_dir +
                   "is detected as a valid directory. Skipping Analysis.")
-    else:
-        #If Directory is not present, then it gets created here and the code
-        #is ran through the analyzer
-        os.mkdir(save_dir)
-        
-        #Importing Config File settings here
-        with io.open(flist_config[j], 'r') as stream:
-            data_loaded = yaml.load(stream)
-        
-        #Call main code here
-        cc.analyzer(flist[i],
-                data_loaded['l'],
-                data_loaded['cmin'],
-                data_loaded['step'],
-                data_loaded['beta'],
-                data_loaded['clump_sizing'],
-                save_dir)
-"""
+        else:
+            #If Directory is not present, then it gets created here and the code
+            #is ran through the analyzer
+            os.mkdir(save_dir)
+            
+            #Importing Config File settings here
+            with io.open(flist_config[j], 'r') as stream:
+                data_loaded = yaml.load(stream)
+            
+            #Call main code here
+            cc.analyzer(flist[i],
+                    data_loaded['l'],
+                    data_loaded['cmin'],
+                    data_loaded['step'],
+                    data_loaded['beta'],
+                    data_loaded['clump_sizing'],
+                    save_dir)
