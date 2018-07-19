ae90326e96fed48ab5e02a6407e940c79cb4ba0f
Samuel Fielder <sdf7@telus.net>
2018-07-19 14:23:42 -0600

    all overwrites set to true

--- clump_code_1/initialize.py
+++ clump_code_1/initialize.py
@@ -87,8 +87,8 @@ Overwrite Protection Here:
     - Setting any of the following values to TRUE will overwrite files
       even if files and directories are found by the code to exist.
 """
-overwrite_analyzer = False
-overwrite_header = False
+overwrite_analyzer = True
+overwrite_header = True
 overwrite_timestep_plots = True
 overwrite_fiducial_plots = True
 
