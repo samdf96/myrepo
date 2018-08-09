f189813134d69cb5f10606c5858ebb6bd0019852
Samuel Fielder <sdf7@telus.net>
2018-08-09 13:25:01 -0600

    naming search for data change

--- clump_code_1/initialize.py
+++ clump_code_1/initialize.py
@@ -74,7 +74,7 @@ import logging.config
 
 #Creates a list of directories with the appropriate files for analysis
 # THIS WILL NEED TO BE CHANGED FOR THE NEWER DESIGN SIMULATIONS
-flist = glob.glob('/mnt/bigdata/erosolow/Orion2/*F*/data.*.hdf5')
+flist = glob.glob('/mnt/bigdata/erosolow/Orion2/**/data.*.hdf5')
 
 #This is to filter out the timestamps that we want to analyze over
 data_check_list = ['0060','0070','0080','0090','0100']
